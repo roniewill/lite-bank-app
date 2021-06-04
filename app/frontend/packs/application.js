157d282c77a765c6b89144bbda9f09c3c788c45d
Ramon William <roniewill@gmail.com>
2021-06-04 18:40:39 -0300

    Added and config frontend with bootstrap and devise

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -8,6 +8,8 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import '../js/bootstrap_js_files.js'  
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
