f694bed886f09f53e9fcf920aad9f26bb934b233
Ramon William <roniewill@gmail.com>
2021-06-10 18:14:55 -0300

    Add validations on model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,19 @@
 class Transaction < ApplicationRecord
   belongs_to :bank_account
+
+  TRANSACTION_TYPES = ["saque","depósito", "transferência"]
+
+  validates :bank_account, presence: true
+  validates :amount, presence: true, numericality: true
+  validates :transaction_type, presence: true, inclusion: { in: TRANSACTION_TYPES}
+  validates :transaction_number, presence: true, uniqueness: true
+
+  before_validation :load_defaults
+
+  def load_defaults
+    if self.new_record?
+      self.transaction_number = SecureRandom.uuid
+    end
+  end
+
 end
