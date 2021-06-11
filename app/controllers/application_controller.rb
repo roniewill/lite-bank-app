26befd1de6ecd99608c265d41e36fb2f778737fc
Ramon William <roniewill@gmail.com>
2021-06-11 00:23:12 -0300

    Fixing insert name on Devise User model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up) do |user|
+        user.permit(:name, :email, :password, :password_confirmation)
+      end
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,4 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :bank_accounts
-
-  validates :name, presence: true
 end
