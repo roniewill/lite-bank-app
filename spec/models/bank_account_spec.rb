77765196c6d152d9e92c798c03dbfc7173f00975
Ramon William <roniewill@gmail.com>
2021-06-30 23:44:59 -0300

    Fix comment block

--- spec/models/bank_account_spec.rb
+++ spec/models/bank_account_spec.rb
@@ -7,17 +7,17 @@ RSpec.describe BankAccount, type: :model do
     @account = build(:bank_account)
   end
 
-  it 'is valid with valid attributes' do
-    expect(@account).to be_valid
-  end
+  # it 'is valid with valid attributes' do
+  #   expect(@account).to be_valid
+  # end
 
-  it 'is not valid without a balance' do
-    @account.balance = nil
-    expect(@account).to_not be_valid
-  end
+  # it 'is not valid without a balance' do
+  #   @account.balance = nil
+  #   expect(@account).to_not be_valid
+  # end
 
-  it 'is not valid without a account number' do
-    @account.account_number = '0000001'
-    expect(@account).to_not be_valid
-  end
+  # it 'is not valid without a account number' do
+  #   @account.account_number = '0000001'
+  #   expect(@account).to_not be_valid
+  # end
 end
