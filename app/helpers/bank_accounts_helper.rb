4016ccc11d481e20236ac872f36f31f7fa7e6127
Ramon William <roniewill@gmail.com>
2021-07-01 20:05:31 -0300

    Refactor helper method

--- app/helpers/bank_accounts_helper.rb
+++ app/helpers/bank_accounts_helper.rb
@@ -2,6 +2,6 @@
 
 module BankAccountsHelper
   def format_value_BRL(valor)
-    ActionController::Base.helpers.number_to_currency(valor, unit: 'R$ ', separator: ',', delimiter: '.')
+    number_to_currency(valor, unit: 'R$ ', separator: ',', delimiter: '.')
   end
 end
