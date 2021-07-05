b8e54701cf36bbf4fef3211c1915bc7862a66f17
Ramon William <roniewill@gmail.com>
2021-07-04 21:04:08 -0300

    Refactor transaction and implements fee

--- app/services/bank_accounts/perform_transaction.rb
+++ app/services/bank_accounts/perform_transaction.rb
@@ -14,6 +14,8 @@ module BankAccounts
       @bank_account_id = params.bank_account_id
       @account_sender = BankAccount.find_by(account_number: params.account_sender)
       @receiver_account = BankAccount.find_by(id: @bank_account_id)
+      @fee = ::BankAccounts::Fee.call(transaction_type: params.transaction_type,
+        amount: params.amount.to_f)
     end
 
     def call
@@ -23,7 +25,7 @@ module BankAccounts
           amount: @amount,
           transaction_type: @transaction_type,
           account_sender: @account_sender.account_number,
-          fee: get_fee
+          fee: @fee
         )
 
         case @transaction_type
@@ -32,34 +34,12 @@ module BankAccounts
         when 'deposit'
           @account_sender.update!(balance: @account_sender.balance.to_f + @amount)
         when 'transfer'
-          @account_sender.update!(balance: (@account_sender.balance.to_f - (@amount + get_fee)))
+          @account_sender.update!(balance: (@account_sender.balance.to_f - (@amount + @fee)))
           @receiver_account.update!(balance: @receiver_account.balance.to_f + @amount)
         end
       end
 
       @account_sender
-    end
-
-    private
-
-    def get_fee
-      if @transaction_type == 'transfer'
-        return generate_fee + ADIOTIONAL_FEE if @amount >= 1000
-
-        generate_fee
-      else
-        0
-      end
-    end
-
-    def generate_fee
-      return BASE_FEE if working_hour?(Time.current)
-
-      HIGHER_FEE
-    end
-
-    def working_hour?(time)
-      time.on_weekday? && time.hour.in?(BUSINESS_HOURS)
     end
   end
 end
